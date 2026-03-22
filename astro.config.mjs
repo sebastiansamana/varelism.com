ff7c642afdda8dd116abd7cc22f7a3c9f515f994
Sebastian Samana <bastivarela@icloud.com>
2026-03-22 17:28:57 +0000

    Fix Pages deployment and update holding page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://USERNAME.github.io',
-  base: process.env.BASE_PATH ?? '/',
+  site: 'https://sebastiansamana.com',
+  base: process.env.BASE_PATH || '/',
   output: 'static',
 });
